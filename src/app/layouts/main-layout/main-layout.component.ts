55d632ea6020b7265bce44c425f3ac217f8f16d7
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-01 14:49:43 +0000

    feat: implement application main layout component with child component sidebar

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -6,6 +6,6 @@ import { SidebarComponent } from '../../components/sidebar/sidebar.component';
   selector: 'app-main-layout',
   imports: [RouterOutlet, SidebarComponent],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css'
+  styleUrl: './main-layout.component.css',
 })
-export class MainLayoutComponent { }
+export class MainLayoutComponent {}
